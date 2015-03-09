f3a2ff038a0fe6140447f5db6bbce77a9aadc169
Jeremie Lasalle Ratelle <jeremie.ratelle@gmail.com>
2015-03-09 12:54:31 -0400

    Add spawn options to timing

--- src/timing.erl
+++ src/timing.erl
@@ -3,7 +3,8 @@
 -export([
     function/1,
     function/2,
-    function/3
+    function/3,
+    function/4
 ]).
 
 -define(N, 20000).
@@ -17,7 +18,10 @@ function(Fun, N) ->
     function(Fun, N, ?P).
 
 function(Fun, N, P) ->
-    function_spawn_loop(self(), Fun, N, P),
+    function(Fun, N, P, []).
+
+function(Fun, N, P, Opts) ->
+    function_spawn_loop(self(), Fun, N, P, Opts),
     Samples = lists:append(receive_loop(P)),
     bear:get_statistics(Samples).
 
@@ -27,11 +31,11 @@ function_loop(_Fun, 0) ->
 function_loop(Fun, N) ->
     [function_time(Fun) | function_loop(Fun, N - 1)].
 
-function_spawn_loop(_Pid, _Fun, _N, 0) ->
+function_spawn_loop(_Pid, _Fun, _N, 0, _Opts) ->
     ok;
-function_spawn_loop(Pid, Fun, N, P) ->
-    spawn_link(fun () -> Pid ! function_loop(Fun, trunc(N / P)) end),
-    function_spawn_loop(Pid, Fun, N, P - 1).
+function_spawn_loop(Pid, Fun, N, P, Opts) ->
+    spawn_opt(fun () -> Pid ! function_loop(Fun, trunc(N / P)) end, [link] ++ Opts),
+    function_spawn_loop(Pid, Fun, N, P - 1, Opts).
 
 function_time(Fun) ->
     Timestamp = os:timestamp(),
